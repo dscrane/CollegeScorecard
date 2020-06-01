c2ca162dc4bda297e3a15701e62c5e343fe2d60a
dscrane <daegancrane@gmail.com>
2020-06-01 14:21:55 -0400

    working on fixing the module issues

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { schoolCardQuery } from "./API/queries";
 
-const testButton = document.querySelector("test_button");
+const testButton = document.querySelector(".test_button");
 testButton.addEventListener("click", schoolCardQuery());
 
 // Display the scorecards on the webpage
