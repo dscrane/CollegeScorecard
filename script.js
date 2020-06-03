d6aa2fd8a9d588685bbb20c1f2e5e398fe801b03
dscrane <daegancrane@gmail.com>
2020-06-03 12:35:52 -0400

    made changes to the implimetation of the api calls

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-import { schoolCardQuery } from "./API/queries";
+import { basicScorecard } from "./api/collegeScorecard/queries.js";
+import { handleScorecard, displayScorecard } from "../../../../utils";
+import { testImgs } from "./lib";
 
 const testButton = document.querySelector(".test_button");
-testButton.addEventListener("click", schoolCardQuery());
 
-// Display the scorecards on the webpage
+testButton.addEventListener("click", () => {
+  console.log("button clicked");
+  basicScorecard()
+    .then((scorecardPromise) => scorecardPromise.results)
+    .then((scorecardData) => handleScorecard(scorecardData, testImgs))
+    .then((cleanedScorecardData) => displayScorecard(cleanedScorecardData));
+});
