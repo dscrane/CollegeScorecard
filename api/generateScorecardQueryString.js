29337f17aa3cd24b2aa240a8931a95b87535ac4b
dscrane <daegancrane@gmail.com>
2020-06-29 13:12:48 -0400

    still working on the new queries

--- api/generateScorecardQueryString.js
+++ api/generateScorecardQueryString.js
@@ -15,9 +15,9 @@ export const generateScorecardQureyString = (querySpecs) => {
   /* --- run the query based on the specific information needed for the gallery card display --- */
   if (isBasicQuery) {
     // get the search value from the input form
-    const searchValue = document.querySelector(".search__input").value;
+    const inputValue = document.querySelector(".search__input").value;
     // Reset the gallery view if no search parameters are entered in the search bar
-    if (searchValue === "") {
+    if (inputValue === "") {
       document.querySelector(".page__row-gallery").innerHTML = "";
       document.querySelector(".page__row-gallery").style.display = "none";
       document.querySelector(".more__results").style.display = "none";
@@ -27,24 +27,30 @@ export const generateScorecardQureyString = (querySpecs) => {
     const page = `page=${currentPage}`;
     const perPage = `per_page=${8}`;
     const institutionType = `school.degrees_awarded.predominant=2,3`;
-    const citySearch = searchValue
-    .replace(/[,]{1}[\s]?/, ",")
-    .split(",");    
+    const searchValues = inputValue.replace(/[,]{1}[\s]?/, ",").split(",");
     let searchParameter;
 
-    console.log(citySearch);
-    const paramsList = citySearch.map(city => {
-      const queryCity = city.replace(" ", "%20");
+    console.log("[searchValues]: ", searchValues);
+    const cityParams = searchValues.map((searchValue) => {
+      const queryCity = searchValue.replace(" ", "%20");
       searchParameter = `school.city=${queryCity}`;
       return `${fields}&${searchParameter}&${page}&${perPage}&${institutionType}&${apiKey}`;
     });
 
-    const queryStrings = paramsList.map(params => {
+    const nameParams = searchValues.map((searchValue) => {
+      const queryName = searchValue.replace(" ", "%20");
+      searchParameter = `school.name=${queryName}`;
+      return `${fields}&${searchParameter}&${page}&${perPage}&${institutionType}&${apiKey}`;
+    });
+
+    const searchParams = cityParams.concat(nameParams);
+    console.log("[searchParams]: ", searchParams);
+
+    const queryStrings = searchParams.map((params) => {
       return `${scorecardUrl}?${params}`;
     });
-    console.log(queryStrings)
     return queryStrings;
-  
+
     // params = `${fields}&${searchParameter}&${page}&${perPage}&${institutionType}&${apiKey}`;
     /* --- run the query based on the data needed for the larger modal and subsections --- */
   } else {
